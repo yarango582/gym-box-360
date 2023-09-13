145b5ad07f8da90a8764ad11350f91fe78ba5c78
yarangodev <yeisonarango03@gmail.com>
2023-09-13 16:23:34 -0500

    feat: configure types of env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,10 @@
 /// <reference types="vite/client" />
 
-VITE_APP_BASE_URL = 'https://api-gymbox360.zeabur.app/';
+interface ImportMetaEnv {
+    readonly VITE_APP_TITLE: string
+    readonly VITE_APP_BASE_URL: string
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
